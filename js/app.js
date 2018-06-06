4a33cd5c261b36dab8e8edf987571d7bd3922fc6
Andrielle Hillis <andriellerhillis@gmail.com>
2018-06-06 16:58:13 -0500

    Successfully added the Character class.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+//I have decided that I'm going to make a Character class on which both the enemies and the players are based, since both of their share a lot of the same functionality
+class Character{
+    constructor(x = 100, y = 100, src = 'images/char-boy.png'){
+        this.x = x;
+        this.y = y;
+        this.sprite = new Image();
+        this.sprite.src = src;
+    }
+
+    handleInput(keyCode){
+        switch(keyCode){
+            case 'up':
+                this.y -= (this.y > 0) ? 85 : 0;
+                break;
+            case 'down':
+                this.y += (this.y < 300) ? 85 : 0;
+                break;
+            case 'right':
+                this.x += (this.x < 400) ? 100 : 0;
+                break;
+            default:
+                this.x -= (this.x > 0) ? 100 : 0;
+        }
+    }
+
+    update(){
+        console.log(this.x);
+    }
+
+    render(){
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
 // Enemies our player must avoid
 const Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -24,41 +57,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function(x, y){
-    this.x = x;
-    this.y = y;
-    this.sprite = new Image();
-    this.sprite.src = "images/char-boy.png";
-};
 
-Player.prototype.handleInput = function(keyCode){
-    switch(keyCode){
-        case 'up':
-            this.y -= (this.y > 0) ? 85 : 0;
-            break;
-        case 'down':
-            this.y += (this.y < 300) ? 85 : 0;
-            break;
-        case 'right':
-            this.x += (this.x < 400) ? 100 : 0;
-            break;
-        default:
-            this.x -= (this.x > 0) ? 100 : 0;
+class Player extends Character{
+    constructor(x, y, src){
+        super(x, y, src);
     }
-};
+}
 
-Player.prototype.update = function(){
-    console.log(this.x);
-};
-
-Player.prototype.render = function(){
-    ctx.drawImage(this.sprite, this.x, this.y);
-};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
-let player = new Player(100, 125);
+let player = new Player(100, 125, "images/char-cat-girl.png");
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
